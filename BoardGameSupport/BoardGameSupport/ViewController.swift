c05b5a48c7f27ffdb4b801ffcc95540c3d1f2bd6
Jay <Jay510248@gmail.com>
2024-03-18 15:56:21 +0800

    wip

---
+++ BoardGameSupport/BoardGameSupport/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BoardGameSupport
+//
+//  Created by 禾智 on 2024/3/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
