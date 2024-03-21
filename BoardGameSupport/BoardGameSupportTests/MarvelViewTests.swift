f04dc000a831eb5bcb7d9fcda7f031e88789247e
Jay <Jay510248@gmail.com>
2024-03-21 16:47:01 +0800

    [Test] 新增測試預設值

--- BoardGameSupport/BoardGameSupportTests/MarvelViewTests.swift
+++ BoardGameSupport/BoardGameSupportTests/MarvelViewTests.swift
@@ -5,4 +5,26 @@
 //  Created by 禾智 on 2024/3/21.
 //
 
-import Foundation
+import XCTest
+@testable import BoardGameSupport
+
+final class MarvelViewTests: XCTestCase {
+
+    var viewModel: MarvelViewModel!
+    
+    override func setUpWithError() throws {
+        viewModel = MarvelViewModel()
+    }
+
+    override func tearDownWithError() throws {
+        viewModel = nil
+    }
+
+    func testIndexDefault() {
+        let testIndex = viewModel.output.index.value
+        XCTAssertEqual(testIndex, 0)
+    }
+}
+
+
+
