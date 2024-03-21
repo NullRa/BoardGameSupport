d61c8b4d479311b52ec0affb056902dfcefc7623
Jay <Jay510248@gmail.com>
2024-03-21 16:47:46 +0800

    [Fix] 修正index預設值錯誤

--- BoardGameSupport/BoardGameSupport/ViewModel/MarvelViewModel.swift
+++ BoardGameSupport/BoardGameSupport/ViewModel/MarvelViewModel.swift
@@ -6,22 +6,30 @@
 //
 
 import Foundation
+import RxSwift
+import RxRelay
 
-
-//class HomeViewModel: ViewModelType {
-//    
-//    private let disposeBag = DisposeBag()
-//    
-//    struct Input {}
-//    
-//    struct Output {}
-//    
-//    let input: Input
-//    
-//    let output: Output
-//    
-//    init() {
-//        self.input = Input()
-//        self.output = Output()
-//    }
-//}
+class MarvelViewModel: ViewModelType {
+    private var testArray: [String] = ["default" ,"test1", "test2", "test3", "test4", "test5", "random"]
+//    private let testName = BehaviorRelay(value: "self.testArray[0]")
+    private let testIndex = BehaviorRelay(value: 0)
+    
+    private let disposeBag = DisposeBag()
+    
+    struct Input {}
+    
+    struct Output {
+        let index: BehaviorRelay<Int>
+    }
+    
+    let input: Input
+    
+    let output: Output
+    
+    init() {
+        self.input = Input()
+        self.output = Output(
+            index: testIndex
+        )
+    }
+}
